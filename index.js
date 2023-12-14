35d5c517b24366aff4b200b221588bd5cce2bef5
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-12-14 17:27:41 -0300

    Cambios funcion html

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  cors()
   next();
 })
 app.use(express.urlencoded({ extended: false }));
