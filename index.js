7e06fb33deaad08fb6c8afbc5cc378c63ee123fb
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-12-14 17:27:00 -0300

    Cambios funcion html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { generarListadoHtml, generararExcel } from './funciones.js'
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
