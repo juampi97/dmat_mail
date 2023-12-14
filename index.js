3484a9e94cd46568642e5bf2db9a918d37084c3f
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-12-14 17:25:50 -0300

    Cambios funcion html

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ import { generarListadoHtml, generararExcel } from './funciones.js'
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
